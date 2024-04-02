2103fb558e560cc690509b0d4d1837f132de5a8c
vangarde03 <alexcobra7743@gmail.com>
2024-04-01 23:13:27 -0400

    turend in to json object

--- api/index.ts
+++ api/index.ts
@@ -53,8 +53,7 @@ app.post("/query", async (req, res) => {
   const { query } = req.body;
   try {
     const result = await executeQuery(query);
-    console.log(result);
-    res.send(result); // Send the raw response data without transformation
+    res.send(result.json()); // Send the raw response data without transformation
   } catch (error) {
     console.error("Error processing query:", error);
     res.status(500).send("Internal server error"); // Send an error message without JSON formatting
